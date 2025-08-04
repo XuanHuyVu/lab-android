a659e61a63c87b7cbcb9c748ec8f343bb19c18c4
VictorFlorence <vuhuy25092004@gmail.com>
2025-08-04 23:42:00 +0700

    add lab2

---
+++ lab2/app/src/main/java/com/example/demo_cycle_life/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.demo_cycle_life;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    Button btncall;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this, "CR424 - onCreate()", Toast.LENGTH_SHORT).show();
+        btncall = findViewById(R.id.btncall);
+        btncall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(MainActivity.this, Subactivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
